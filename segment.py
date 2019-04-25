49bfeafc749dbe357c7fec61cda274e4d4986c57
Wester Coenraads <info@lokkij.com>
2019-04-25 14:14:05 +0200

    Remove the black border around fragments using snakes.

--- segment.py
+++ segment.py
@@ -3,13 +3,14 @@ import skimage
 from skimage import segmentation, morphology
 
 img = skimage.io.imread('test-images/normal-fragment.jpg')
-initial_levelset = segmentation.circle_level_set(img.shape, radius=50)
+img2 = np.copy(img)
 preprocessed = segmentation.inverse_gaussian_gradient(img, alpha=200, sigma=1)
 mask = segmentation.flood(preprocessed, (int(img.shape[0] / 2), int(img.shape[1] / 2)), tolerance=0.5)
 mask = morphology.closing(mask, selem=morphology.disk(4))
 # contour = segmentation.find_boundaries(mask)
-img[mask == False] = 0
-# segmented = segmentation.morphological_chan_vese(img, 10, init_level_set=mask)
-
-skimage.io.imshow(img)
+img2[mask == False] = 0
+segmented = segmentation.morphological_chan_vese(img2, 15, init_level_set=mask)
+img2 = np.copy(img)
+img2[segmented == False] = 150
+skimage.io.imshow(img2)
 skimage.io.show()
